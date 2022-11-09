14e19bc9a3e0e1919e8a87ad2b239535d0a91c3c
BartonThomas <99219127+BartonThomas@users.noreply.github.com>
2022-11-08 21:58:04 -0300

    exercício comentado

--- SEMANA_03/EXERCICIOS/exercicio/exercicio.ino
+++ SEMANA_03/EXERCICIOS/exercicio/exercicio.ino
@@ -1,3 +1,4 @@
+/*Arrays utilizadas para guardar a informação de todos os LEDs ascendidos e sons tocados*/
 int music[50] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int leds[50] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int placement = 0;
@@ -5,43 +6,50 @@ int placement = 0;
 void setup() {
   Serial.begin(9600);
 
-  /*LEDs*/
+  /*Setando pinos dos LEDs*/
   pinMode(15, OUTPUT);
   pinMode(16, OUTPUT);
   pinMode(17, OUTPUT);
   pinMode(18, OUTPUT);
 
-  /*Buzzer*/
+  /*Setando pinos do buzzer*/
   pinMode(7, OUTPUT);
 
-  /*Buttons*/
+  /*Setando pinos dos botões*/
   pinMode(20, INPUT_PULLUP);
   pinMode(21, INPUT_PULLUP);
 }
 
 void loop() {
+  /*Variável utilizada para receber o código binário de outras funções*/
   int ldrBinary;
+
+  /*Leitura dos botões de update e reset*/
   int buttonReset = digitalRead(20);
   int buttonUpdate = digitalRead(21);
 
+  /*Verificação se o botão para fazer o update foi apertado*/
   if(buttonUpdate == LOW){
     ldrBinary = update();
     change(ldrBinary);
   }
 
+  /*Verificação se o botão de reset foi apertado*/
   if(buttonReset == LOW){
     ldrBinary = 2222;
     change(ldrBinary);
     song();
   }
-  delay(500);
+  delay(500); /*Delay de 0,5 segundos para não quebrar o esp32*/
 }
 
 int update() {
-  const int place = placement;
-  int ldrRead = analogRead(14);
-  ldrRead = map(ldrRead, 0, 4095, 0, 15);
+  const int place = placement; /*Constante utilizada para inserir novos valores de som na array msuic[] e leds[]*/
+  int ldrRead = analogRead(14); /*Leitura dos dados recebidos pelo sensor*/
+  ldrRead = map(ldrRead, 0, 4095, 0, 15); /*Mapeamento do número recebido para que fique entre 0 e 15*/
   int ldrReturn;
+  /*Verificações do número lido pelo sensor convertido de 1 a 15, decisão de sons a serem tocados para cada combinação*/
+  /*e conversão para um código binário dependendo do número*/
   switch(ldrRead){
     case 0:
     ldrReturn = 2222;
@@ -157,6 +165,7 @@ int update() {
 }
 
 void change(int ldrBinary){
+  /*Série de verificações do código binário para detectar quais LEDs ativar e desativar*/
   if(ldrBinary%10 == 1){
     digitalWrite(15, HIGH);
   }
@@ -192,6 +201,7 @@ void change(int ldrBinary){
 }
 
 void song(){
+  /*Inicia um loop para tocar todos os sons e acionar todos os leds previamente ativados*/
   for(int i = 0; i <= 50; i++){
     const int x = i;
     int z = music[x];
